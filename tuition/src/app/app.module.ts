91b190e5ac4a986ad5c241cb21f981e01c4df3bd
muhammadzaim <muhammadzaim@wavelet.net>
2022-06-28 15:27:07 +0800

    #2 Fixed CORS problem, added Student comp

--- tuition/src/app/app.module.ts
+++ tuition/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { StudentCreateComponent } from './student/student-create/student-create.component';
+import { StudentEditComponent } from './student/student-edit/student-edit.component';
+import { StudentViewComponent } from './student/student-view/student-view.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    StudentCreateComponent,
+    StudentEditComponent,
+    StudentViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

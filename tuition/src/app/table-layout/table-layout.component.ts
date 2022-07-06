8d598a3f91a2b68d9fd9daa598452068cb37b586
miazocool <zaimsaharudin@gmail.com>
2022-07-07 00:25:42 +0800

    #22 Added basic animation in create

--- tuition/src/app/table-layout/table-layout.component.ts
+++ tuition/src/app/table-layout/table-layout.component.ts
@@ -1,12 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-table-layout',
   templateUrl: './table-layout.component.html',
-  styleUrls: ['./table-layout.component.css']
+  styleUrls: ['./table-layout.component.css'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        //backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        //backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('0.5s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class TableLayoutComponent implements OnInit {
+  isOpen = true;
 
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
   constructor() { }
 
   ngOnInit(): void {

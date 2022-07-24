9d36522683d5215e71808d25429e4ccb1eb745b0
miazocool <zaimsaharudin@gmail.com>
2022-07-24 11:42:49 +0800

    Fixed routing issue

--- tuition/src/app/components-secondary/sidebar/sidebar.component.ts
+++ tuition/src/app/components-secondary/sidebar/sidebar.component.ts
@@ -8,15 +8,15 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/student', title: 'Student List',  icon:'content_paste', class: '' },
-    { path: '/table-layout', title: 'Table-Layout',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    // { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/layout/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/layout/student', title: 'Student List',  icon:'content_paste', class: '' },
+    { path: '/layout/table-layout', title: 'Table-Layout',  icon:'content_paste', class: '' },
+    // { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    // { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    // { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
+    // { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    // { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
 
 @Component({

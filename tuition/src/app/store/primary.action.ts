07a626f4d3102571056cdd54d02299cdb5698382
miazocool <zaimsaharudin@gmail.com>
2022-07-28 23:11:48 +0800

    Added effect

--- tuition/src/app/store/primary.action.ts
+++ tuition/src/app/store/primary.action.ts
@@ -1,9 +1,27 @@
 import { createAction } from '@ngrx/store';
 
-const STUDENT_GET_ALL = '[Student Component] Retrieve data'
-const LEVEL_GET_ALL = '[Student Component] Retrieve data'
-const SUBJECT_GET_ALL = '[Student Component] Retrieve data'
+const STUDENT_GET_ALL_INIT = '[Student Component] Retrieve data Init'
+const STUDENT_GET_ALL_SUCCESS = '[Student Component] Retrieve data Success'
+const STUDENT_GET_ALL_FAILURE = '[Student Component] Retrieve data Failure'
 
-export const studentGetAllAction = createAction(STUDENT_GET_ALL);
-export const levelGetAllAction = createAction(LEVEL_GET_ALL);
-export const subjectGetAllAction = createAction(SUBJECT_GET_ALL);
+const LEVEL_GET_ALL_INIT = '[Student Component] Retrieve data'
+const LEVEL_GET_ALL_SUCCESS = '[Student Component] Retrieve data Success'
+const LEVEL_GET_ALL_FAILURE = '[Student Component] Retrieve data Failure'
+
+const SUBJECT_GET_ALL_INIT = '[Student Component] Retrieve data'
+
+const SUBJECT_GET_ALL_SUCCESS = '[Student Component] Retrieve data Success'
+const SUBJECT_GET_ALL_FAILURE = '[Student Component] Retrieve data Failure'
+
+export const studentGetAllActionInit = createAction(STUDENT_GET_ALL_INIT);
+export const studentGetAllActionSuccess = createAction(STUDENT_GET_ALL_SUCCESS);
+export const studentGetAllActionFailure = createAction(STUDENT_GET_ALL_FAILURE);
+
+export const levelGetAllActionInit = createAction(LEVEL_GET_ALL_INIT);
+export const levelGetAllActionSuccess = createAction(LEVEL_GET_ALL_SUCCESS);
+export const levelGetAllActionFailure = createAction(LEVEL_GET_ALL_FAILURE);
+
+export const subjectGetAllActionInit = createAction(SUBJECT_GET_ALL_INIT);
+export const subjectGetAllActionSuccess = createAction(SUBJECT_GET_ALL_SUCCESS);
+export const subjectGetAllActionFailure = createAction(SUBJECT_GET_ALL_FAILURE);
+
---
+++ tuition/src/app/store/primary.effect.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { LevelService } from 'app/_services/level.service';
+import { StudentService } from 'app/_services/student.service';
+import { SubjectService } from 'app/_services/subject.service';
+import { EMPTY } from 'rxjs';
+import { createEffect } from '@ngrx/effects';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { levelGetAllActionInit, levelGetAllActionSuccess, studentGetAllActionInit, studentGetAllActionSuccess, subjectGetAllActionInit, subjectGetAllActionSuccess } from './primary.action';
+
+@Injectable()
+export class StudentEffects {
+
+  loadStudent$ = createEffect(() => {
+    return this.actions$
+      .pipe(
+        ofType(studentGetAllActionInit),
+        mergeMap(() => this.student$.getStudentAll()
+          .pipe(
+            map(student => studentGetAllActionSuccess()),
+            catchError(() => EMPTY)
+          )))
+  })
+
+  loadLevel$ = createEffect(() => {
+    return this.actions$
+      .pipe(
+        ofType(levelGetAllActionInit),
+        mergeMap(() => this.level$.getLevelAll()
+          .pipe(
+            map(level => levelGetAllActionSuccess()),
+            catchError(() => EMPTY)
+          )))
+  })
+
+  loadSubject$ = createEffect(() => {
+    return this.actions$
+      .pipe(
+        ofType(subjectGetAllActionInit),
+        mergeMap(() => this.subject$.getSubjectAll()
+          .pipe(
+            map(subject => subjectGetAllActionSuccess()),
+            catchError(() => EMPTY)
+          )))
+  })
+
+  constructor(
+    private actions$: Actions,
+    private student$: StudentService,
+    private level$: LevelService,
+    private subject$: SubjectService
+  ) { }
+}
